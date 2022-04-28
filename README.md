b1469d7a966f77a7f59ee8c213ed47ed7b395ab7
Tahir Turgut <tahirturgutt@outlook.com>
2022-04-29 01:31:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E-commerce website
+ Platform to sell e-pins - c2a.store 
---
+++ unit-tests/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    rootDir: '../..',
+    collectCoverage: true,
+    testURL: 'http://localhost',
+    testEnvironment: 'jsdom',
+    testMatch: [
+    '<rootDir>/unit-tests/*.(spec|test).(j|t)s'
+]
+};
